0fdf3626638bb0d29063649f9e0258848477e4c7
zamods <zamods@outlook.com>
2019-06-20 17:44:26 +0400

    Added Demos for drop shadow and svg scaling. Fixed few bugs.

---
+++ XFSVGUIElement/XFSVGUIElement/MainPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XFSVGUIElement
+{
+    public class MainPageViewModel
+    {
+        public ICommand ButtonClicked { get; private set; }
+
+        public MainPageViewModel(INavigation navigation)
+        {
+            ButtonClicked = new Command<string>(commandParameter => NavigateToPage(commandParameter, navigation));
+        }
+
+        private void NavigateToPage(string commandParameter, INavigation navigation)
+        {
+            switch (commandParameter)
+            {
+                case "FewExamplesDemo":
+                    navigation.PushAsync(new FewExamplesDemo());
+                    break;
+
+                case "ScaleDemo":
+                    navigation.PushAsync(new ScalingSvgDemo());
+                    break;
+
+                case "DropShadowDemo":
+                    navigation.PushAsync(new DropShadowDemo());
+                    break;
+            }
+        }
+    }
+}
